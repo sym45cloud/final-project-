718ce3c7635ddf93e815b7fdc7352ad0c2f4d2df
sym45cloud <117885681+sym45cloud@users.noreply.github.com>
2022-11-14 10:41:41 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 radio.setGroup(1)
-radio.setTransmitPower(1)
+radio.setTransmitPower(0.1)
 let Id = 1
-basic.showNumber(Id)
-basic.pause(2000)
-basic.clearScreen()
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    # # # # #
+    # # . # #
+    # # # # #
+    `)
 basic.forever(function () {
     radio.sendNumber(Id)
     basic.pause(200)
